3c40c98aaa8049dfd2c573e38bd098fd43ab70b3
rohitprocode <rohit990797@gmail.com>
2025-06-26 00:38:17 +0530

    Link in next js

---
+++ src/app/link/page.jsx
@@ -0,0 +1,12 @@
+import LearnLink from "@/components/LearnLink";
+
+const Link = () => {
+  return (
+    <div>
+      Ye hai Link Page
+      <LearnLink />
+    </div>
+  );
+};
+
+export default Link;
---
+++ src/components/LearnLink.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+const LearnLink = () => {
+  return (
+    <div>
+      <Link href="/admin/dashboard">Go to Admin Dashboard</Link>
+    </div>
+  );
+};
+
+export default LearnLink;
