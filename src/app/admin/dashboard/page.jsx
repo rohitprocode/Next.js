cdd82e2e12af1f7914879bfbc7951da4b25b1b27
rohitprocode <rohit990797@gmail.com>
2025-06-28 15:05:30 +0530

    Module css

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -1,4 +1,5 @@
 import LearnGlobalCss from "@/components/LearnGlobalCss";
+import LearnModuleCss from "@/components/LearnModuleCSS";
 import LearnUseRouter from "@/components/LearnUseRouter";
 
 const Dashboard = () => {
@@ -7,8 +8,9 @@ const Dashboard = () => {
       <h1>Ye hai Admin Dashboard</h1>
       <LearnUseRouter />
       <LearnGlobalCss />
+      <LearnModuleCss/>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
---
+++ src/components/LearnModuleCSS.jsx
@@ -0,0 +1,12 @@
+import Styles from "../css/LearnModuleCss.module.css";
+const LearnModuleCss = () => {
+  return (
+    <div>
+      <h1 className={`${Styles.rohitBrandColor} ${Styles.brandFontSize}`}>
+        Ye hai Module page
+      </h1>
+    </div>
+  );
+};
+
+export default LearnModuleCss;
